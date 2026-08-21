a9cbb6ce3f599263979d5cef0ec85d8b52f04d26
Scott-Tatham <scottytatham@google.com>
2026-08-20 19:09:05 -0600

    Setup Pages Build + Added a deploy.yaml to build the page on GitHub pages. + Added the page information to astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(
 {
+  site: 'https://Scott-Tatham.github.io',
+  base: '/GroupoSCE',
   vite:
-  {
-    plugins: [tailwindcss()]
-  }
+      {
+        plugins: [tailwindcss()]
+      }
 });
